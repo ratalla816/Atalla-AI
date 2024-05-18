9ebf0850a17d8a6ccbd760b759386358a3d45afd
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2024-05-18 00:51:01 -0500

    updated model

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.post('/', async (req, res) => {
     const response = await openai.completions.create({
         
         // code example from the openai website \\ 
-        model: "gpt-4-turbo-2024-04-09",
+        model: "gpt-4-turbo-preview",
         // model: "gpt-3.5-turbo-instruct",
         // model: "text-davinci-003", // select one of the AI models from the openai website - this one handles text as well as code
       prompt: `${prompt}`,
